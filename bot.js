1084582cffd072c9fca65c923db072849c1523c3
salamus <salamus@yandex.com>
2024-10-09 07:30:17 +0300

    end

--- bot.js
+++ bot.js
@@ -1,40 +1,39 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api'); 
 
-
-const token = '7375700638:AAHe3cnsdQaESFhOtJ7mZmQ0bO9TOo4ebyU';
-const editorChatId = 1137493485;
-
-
+const token = '7156292147:AAHXfddoJhwxkee-TSJSi9-jRa_sS6cLqYw';
+const editorChatId = 167118123;
+//167118123
 const bot = new TelegramBot(token, { polling: true });
 
 
-const welcomeMessage = 'Привет! Вы можете анонимно отправить сообщение редактору. Пожалуйста, введите текст сообщения:';
-
+const welcomeMessage = `анонимный бот медиа ‘сенсуальность’. мы здесь, чтобы выслушать и поддержать.\nканал: @royal_secrets`;
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     console.log(chatId);
-    
-    bot.sendMessage(chatId, welcomeMessage);
-});
 
+
+    
+    
+
+        bot.sendMessage(chatId, 'что вас волнует? расскажите свою историю, фантазию или задайте вопрос сексологу.');
+
+});
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const messageText = msg.text;
 
-        if (messageText.startsWith('/')) {
-            return;
-        }
+    if (messageText.startsWith('/')) {
+        return;
+    }
 
-        // (ID: ${chatId})
-        bot.sendMessage(editorChatId, `Новое анонимное сообщение от пользователя(ID: ${chatId}) :\n${messageText}`);
-        
 
-        bot.sendMessage(chatId, 'Ваше сообщение было отправлено анонимно.');
+    bot.sendMessage(editorChatId, `Новое анонимное сообщение от пользователя (ID: ${chatId}):\n${messageText}`);
+    
 
+    bot.sendMessage(chatId, 'спасибо, что поделились сокровенным.\nмы умеем хранить тайны и при публикации спрячем твою личность под маской белой тигрицы или нефритового дракона   \n\n_чувственные масла для контакта с собой и партнёром_\n**сайт**: [Royal Secrets](https://royal-secrets.ru/)\n**инстаграм**: [royalsecretsss](https://www.instagram.com/royalsecretsss)', { parse_mode: 'Markdown' });
 });
-
 
 bot.on('polling_error', (error) => {
     console.error(error);
